06e4fb471f31947bdbef645c1f65835ba824fae3
Evan Bolton <elbo230@uky.edu>
2023-03-20 13:30:27 -0400

    Initial Commit for v2

---
+++ app/src/main/java/com/example/foodscanner_v2/ui/favorites/FavoritesFragment.kt
@@ -0,0 +1,42 @@
+package com.example.foodscanner_v2.ui.favorites
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.foodscanner_v2.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+
+    private var _binding: FragmentFavoritesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val favoritesViewModel =
+            ViewModelProvider(this).get(FavoritesViewModel::class.java)
+
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        favoritesViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/foodscanner_v2/ui/favorites/FavoritesViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.foodscanner_v2.ui.favorites
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FavoritesViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
