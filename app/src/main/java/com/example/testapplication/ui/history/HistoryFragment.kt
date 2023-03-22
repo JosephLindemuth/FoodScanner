713c108684da25230cff45beaedde0e19812c2df
Joseph Lindemuth <joe@uky.edu>
2023-03-22 15:35:56 -0400

    Got Evan's foodscanner v2 working on my machine

---
+++ app/src/main/java/com/example/testapplication/ui/history/HistoryFragment.kt
@@ -0,0 +1,42 @@
+package com.example.foodscanner_v2.ui.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.testapplication.databinding.FragmentHistoryBinding
+
+class HistoryFragment : Fragment() {
+
+    private var _binding: FragmentHistoryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val historyViewModel =
+            ViewModelProvider(this).get(HistoryViewModel::class.java)
+
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        historyViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
